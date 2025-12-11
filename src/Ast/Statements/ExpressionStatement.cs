c75091d4b416806084c95ea17657118229930b55
Sakata <pichugov-sasha@mail.ru>
2025-12-11 07:20:21 +0300

    Разработчик: пуш пустых и выраженческих инструкций

---
+++ src/Ast/Statements/ExpressionStatement.cs
@@ -0,0 +1,24 @@
+﻿using Ast.Expressions;
+
+namespace Ast.Statements;
+
+/// <summary>
+/// Инструкция для выполнения.
+/// </summary>
+public sealed class ExpressionStatement : Statement
+{
+    public ExpressionStatement(Expression expression)
+    {
+        Expression = expression;
+    }
+
+    /// <summary>
+    /// Выражение для выполнения.
+    /// </summary>
+    public Expression Expression { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
