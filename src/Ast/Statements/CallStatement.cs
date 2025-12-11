ab7b7894dde79d60b4478983af170f4e0e29c6d7
Sakata <pichugov-sasha@mail.ru>
2025-12-11 07:14:21 +0300

    Разработчик: запушил интерфейс и вызов функций/процедур

---
+++ src/Ast/Statements/CallStatement.cs
@@ -0,0 +1,30 @@
+﻿using Ast.Expressions;
+
+namespace Ast.Statements;
+
+/// <summary>
+/// Вызов функции/процедуры.
+/// </summary>
+public class CallStatement : Statement
+{
+    public CallStatement(string name, List<Expression> arguments)
+    {
+        Name = name;
+        Arguments = arguments;
+    }
+
+    /// <summary>
+    /// Имя вызываемой функции/процедуры.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Список аргументов функции/процедуры.
+    /// </summary>
+    public List<Expression> Arguments { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
