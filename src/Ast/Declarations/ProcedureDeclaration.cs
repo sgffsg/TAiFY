3d0eff15a808a634402544e340995220ed282fcb
Sakata <pichugov-sasha@mail.ru>
2025-12-11 06:57:57 +0300

    Разработчик: добавил объявление для функций и процедур

---
+++ src/Ast/Declarations/ProcedureDeclaration.cs
@@ -0,0 +1,36 @@
+﻿using Ast.Statements;
+
+namespace Ast.Declarations;
+
+/// <summary>
+/// Объявление процедуры.
+/// </summary>
+public sealed class ProcedureDeclaration : Declaration
+{
+    public ProcedureDeclaration(string name, List<ParameterDeclaration> parameters, BlockStatement body)
+    {
+        Name = name;
+        Parameters = parameters;
+        Body = body;
+    }
+
+    /// <summary>
+    /// Название процедуры.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Список параметров процедуры.
+    /// </summary>
+    public List<ParameterDeclaration> Parameters { get; }
+
+    /// <summary>
+    /// Тело процедуры.
+    /// </summary>
+    public BlockStatement Body { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
