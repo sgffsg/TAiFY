1c9ac52dc6f62156a84a3f0716efd3b78159dca4
Sakata <pichugov-sasha@mail.ru>
2025-12-11 07:17:40 +0300

    Разработчик: Ввод-вывод

---
+++ src/Ast/Statements/InputStatement.cs
@@ -0,0 +1,22 @@
+﻿namespace Ast.Statements;
+
+/// <summary>
+/// Ввод значений с консоли в переменные.
+/// </summary>
+public sealed class InputStatement : Statement
+{
+    public InputStatement(List<string> variables)
+    {
+        Variables = variables;
+    }
+
+    /// <summary>
+    /// Список переменных для ввода значений.
+    /// </summary>
+    public List<string> Variables { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
