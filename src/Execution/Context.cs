12ac98ef889eb092b7fb65ccd6114e0b534d1b4a
Sakata <pichugov-sasha@mail.ru>
2025-12-11 07:33:51 +0300

    Запушил пустой контекст и scope

---
+++ src/Execution/Context.cs
@@ -0,0 +1,16 @@
+﻿using Ast.Declarations;
+
+namespace Execution;
+
+/// <summary>
+/// Контекст выполнения программы (все переменные, константы, функции).
+/// </summary>
+public class Context
+{
+    private readonly Stack<Scope> scopes = new();
+    private readonly Dictionary<string, decimal> constants = new();
+    private readonly Dictionary<string, FunctionDeclaration> functions = new();
+    private readonly Dictionary<string, ProcedureDeclaration> procedures = new();
+
+
+}
