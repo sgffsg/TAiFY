451c5d24178c8406b67055edbccfda82cc0f41ee
Sakata <pichugov-sasha@mail.ru>
2025-12-11 07:23:11 +0300

    Разработчик: чисто идентефикатор

---
+++ src/Ast/Expressions/IdentifierExpression.cs
@@ -0,0 +1,22 @@
+﻿namespace Ast.Expressions;
+
+/// <summary>
+/// Идентификатор переменной или функции.
+/// </summary>
+public sealed class IdentifierExpression : Expression
+{
+    public IdentifierExpression(string name)
+    {
+        Name = name;
+    }
+
+    /// <summary>
+    /// Имя идентификатора.
+    /// </summary>
+    public string Name { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
