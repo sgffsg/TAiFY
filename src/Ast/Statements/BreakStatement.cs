2f149da748b50cd80da8ec64b7dfa5fad1189b47
Sakata <pichugov-sasha@mail.ru>
2025-12-11 06:56:13 +0300

    Разработчик: добавил выражения для унарных и бинарных операций, добавил инструкции для break/continue

---
+++ src/Ast/Statements/BreakStatement.cs
@@ -0,0 +1,16 @@
+﻿namespace Ast.Statements;
+
+/// <summary>
+/// Прерывание выполнения цикла.
+/// </summary>
+public sealed class BreakStatement : Statement
+{
+    public BreakStatement()
+    {
+    }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
