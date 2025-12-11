d9a12b6120ba1209f9eae33986ec4dda5db191fd
Sakata <pichugov-sasha@mail.ru>
2025-12-11 07:08:33 +0300

    Разработчик: добавил объявления для констант, глобальных переменных

---
+++ src/Ast/Declarations/VariableDeclaration.cs
@@ -0,0 +1,30 @@
+﻿using Ast.Expressions;
+
+namespace Ast.Declarations;
+
+/// <summary>
+/// Выражение переменной.
+/// </summary>
+public sealed class VariableDeclaration : Declaration
+{
+    public VariableDeclaration(string name, Expression? value)
+    {
+        Name = name;
+        Value = value;
+    }
+
+    /// <summary>
+    /// Имя переменной.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Базовое значение переменной.
+    /// </summary>
+    public Expression? Value { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
