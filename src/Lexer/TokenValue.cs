d85222de41f088ce74b16e0806ccf834bce7cb5e
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-16 22:34:03 +0300

    Разработчик: поменял тип объекта у токена на object

--- src/Lexer/TokenValue.cs
+++ src/Lexer/TokenValue.cs
@@ -4,15 +4,16 @@
     {
         private readonly object value;
 
-        public TokenValue(string value) => this.value = value;
-
-        public TokenValue(decimal value) => this.value = value;
+        public TokenValue(object value) => this.value = value;
 
         public override string ToString() =>
             value switch
             {
                 string s => s,
+                int i => i.ToString(),
+                double d => d.ToString(),
                 decimal d => d.ToString(),
+                bool b => b.ToString(),
                 _ => throw new NotImplementedException()
             };
 
@@ -20,8 +21,9 @@
             value switch
             {
                 string s => double.Parse(s),
-                double d => d,
                 int i => i,
+                double d => d,
+                decimal d => Convert.ToDouble(d),
                 bool b => Convert.ToDouble(b),
                 _ => throw new NotImplementedException()
             };
@@ -30,7 +32,10 @@
             value switch
             {
                 string s => decimal.Parse(s),
+                int i => i,
+                double d => Convert.ToDecimal(d),
                 decimal d => d,
+                bool b => Convert.ToDecimal(b),
                 _ => throw new NotImplementedException()
             };
 
