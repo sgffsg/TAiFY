630d90e1c51e68934e6f4123bcbbdc64d620266b
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-17 01:39:52 +0300

    Разработчик: добавил тест на объявление переменной

--- tests/Parser.UnitTests/ParseTopLevelStatementsTests.cs
+++ tests/Parser.UnitTests/ParseTopLevelStatementsTests.cs
@@ -5,7 +5,7 @@ namespace Parser.UnitTests;
 public class ParseTopLevelStatementsTests
 {
     [Fact]
-    public void Parse_variable_declaration()
+    public void Parse_int_variable_declaration()
     {
         string code = @"ЦИФЕРКА x = 5;";
         Context context = new();
@@ -15,6 +15,19 @@ public class ParseTopLevelStatementsTests
         parser.ParseProgram();
         Assert.True(context.Exists("x"));
         Assert.Equal(5, context.GetValue("x"));
+    }
+
+    [Fact]
+    public void Parse_float_variable_declaration()
+    {
+        string code = @"ПОЛТОРАШКА x = 5.1;";
+        Context context = new();
+        FakeEnvironment environment = new();
+        Parser parser = new(context, environment, code);
+
+        parser.ParseProgram();
+        Assert.True(context.Exists("x"));
+        Assert.Equal(5.1, context.GetValue("x"));
     }
 
     private void RunBaseTest(string code, double[] inputs, double[] expected)
