0a3af980321c4d27db20cd6ea23905c4516a0222
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-16 16:56:37 +0300

    Разработчик: Добавил класс программы и интерпретатора

---
+++ src/Interpreter/VaibikInterpreter.cs
@@ -0,0 +1,32 @@
+﻿using Execution;
+
+namespace Interpreter;
+
+/// <summary>
+/// Интерпретатор для выполнения программ.
+/// </summary>
+public class VaibikiInterpreter
+{
+    private readonly Context context;
+    private readonly IEnvironment environment;
+
+    public VaibikiInterpreter()
+        : this(new Context(), new ConsoleEnvironment())
+    {
+    }
+
+    public VaibikiInterpreter(Context context, IEnvironment environment)
+    {
+        this.context = context;
+        this.environment = environment;
+    }
+
+    /// <summary>
+    /// Выполняет программу из переданного кода.
+    /// </summary>
+    public void Execute(string code)
+    {
+        Parser.Parser parser = new Parser.Parser(context, environment, code);
+        parser.ExecuteProgram();
+    }
+}
