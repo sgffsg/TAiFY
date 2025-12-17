ee450d4c5b71be7d6f082c1fb3b7a1bebfee69c1
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-17 04:38:49 +0300

    Разработчик: добавил инструкцию для input, output

---
+++ src/Ast/Statements/InputStatement.cs
@@ -0,0 +1,22 @@
+﻿namespace Ast.Statements;
+
+/// <summary>
+/// Ввод значений с консоли в переменные.
+/// </summary>
+public sealed class InputStatement : Statement
+{
+    public InputStatement(List<string> variableNames)
+    {
+        VariableNames = variableNames;
+    }
+
+    /// <summary>
+    /// Список переменных для ввода значений.
+    /// </summary>
+    public List<string> VariableNames { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
