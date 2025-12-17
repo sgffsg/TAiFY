b3b1b2253af12822fa9da731d5e61fe7a3f6fab7
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-17 04:48:40 +0300

    Разработчик: добавил инструкцию для идентификаторов и литералов

---
+++ src/Ast/Expressions/LiteralExpression.cs
@@ -0,0 +1,22 @@
+﻿namespace Ast.Expressions;
+
+/// <summary>
+/// Литеральное значение (число, строка, булевое значение, null).
+/// </summary>
+public sealed class LiteralExpression : Expression
+{
+    public LiteralExpression(object value)
+    {
+        Value = value;
+    }
+
+    /// <summary>
+    /// Значение литерала.
+    /// </summary>
+    public object Value { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
