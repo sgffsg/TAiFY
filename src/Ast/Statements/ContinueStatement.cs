f68882dc784a2222d0441621cc80bae5cd3f7656
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-17 04:38:10 +0300

    Разработчик: добавил инструкцию для break, continue, return

---
+++ src/Ast/Statements/ContinueStatement.cs
@@ -0,0 +1,16 @@
+﻿namespace Ast.Statements;
+
+/// <summary>
+/// Переход к след. итерации цикла.
+/// </summary>
+public sealed class ContinueStatement : Statement
+{
+    public ContinueStatement()
+    {
+    }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
---
+++ src/Ast/Statements/ReturnStatement.cs
@@ -0,0 +1,24 @@
+﻿using Ast.Expressions;
+
+namespace Ast.Statements;
+
+/// <summary>
+/// Возврат значения из функции.
+/// </summary>
+public sealed class ReturnStatement : Statement
+{
+    public ReturnStatement(Expression value)
+    {
+        Value = value;
+    }
+
+    /// <summary>
+    /// Возвращаемое значение функции.
+    /// </summary>
+    public Expression Value { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
