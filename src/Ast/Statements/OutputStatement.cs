062393c9e244ed3bb9ce6545888e37ea1267be3d
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-17 05:06:46 +0300

    Разработчик: внес все сущности в визитора и добавил ссылку в Execution

--- src/Ast/Statements/OutputStatement.cs
+++ src/Ast/Statements/OutputStatement.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ast.Expressions;
 
-namespace Ast.Statements
+namespace Ast.Statements;
+
+/// <summary>
+/// Вывод значений выражений в консоль.
+/// </summary>
+public sealed class OutputStatement : Statement
 {
-    internal class OutputStatement
+    public OutputStatement(List<Expression> arguments)
     {
+        Arguments = arguments;
+    }
+
+    /// <summary>
+    /// Список выражений для вывода.
+    /// </summary>
+    public List<Expression> Arguments { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
     }
 }
