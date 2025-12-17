8d47ccbeafd8a3e42a981dbc6639de13bd3016f9
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-17 04:39:45 +0300

    Разработчик: добавил инструкцию для присваивания значений

---
+++ src/Ast/Statements/VariableAssignmentStatement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ast.Expressions;
+
+namespace Ast.Statements;
+
+public class VariableAssignmentStatement : Statement
+{
+    public VariableAssignmentStatement(string variableName, Expression value)
+    {
+        VariableName = variableName;
+        Value = value;
+    }
+
+    public string VariableName { get; }
+
+    public Expression Value { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
