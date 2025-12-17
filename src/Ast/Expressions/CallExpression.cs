4a83dc9edb53806341995b0f93c990496ea03590
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-17 04:48:56 +0300

    Разработчик: добавил инструкцию для вызова функции/процедуры

---
+++ src/Ast/Expressions/CallExpression.cs
@@ -0,0 +1,28 @@
+﻿namespace Ast.Expressions;
+
+/// <summary>
+/// Вызов функции/процедуры.
+/// </summary>
+public class CallExpression : Expression
+{
+    public CallExpression(string name, List<Expression> arguments)
+    {
+        Name = name;
+        Arguments = arguments;
+    }
+
+    /// <summary>
+    /// Имя вызываемой функции/процедуры.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Список аргументов функции/процедуры.
+    /// </summary>
+    public List<Expression> Arguments { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
