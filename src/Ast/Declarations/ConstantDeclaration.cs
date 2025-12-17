b9ccf18ff6711b26cf9aa800f3a4fa8fb7b2c22c
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-17 04:22:44 +0300

    Разработчик: добавил объявление константы

---
+++ src/Ast/Declarations/ConstantDeclaration.cs
@@ -0,0 +1,36 @@
+﻿using Ast.Expressions;
+
+namespace Ast.Declarations;
+
+/// <summary>
+/// Объявление константы.
+/// </summary>
+public sealed class ConstantDeclaration : Declaration
+{
+    public ConstantDeclaration(string typeName, string name, Expression value)
+    {
+        TypeName = typeName;
+        Name = name;
+        Value = value;
+    }
+
+    /// <summary>
+    /// Тип константы.
+    /// </summary>
+    public string TypeName { get; }
+
+    /// <summary>
+    /// Имя константы.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Значение константы.
+    /// </summary>
+    public Expression Value { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
