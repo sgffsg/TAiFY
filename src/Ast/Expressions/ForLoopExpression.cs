07c09030decde2fad8b3162c71e505dbc18866e2
Alexander Pichugov <pichugov-sasha@mail.ru>
2025-12-17 09:11:51 +0300

    Разработчик: поправил выражения

--- src/Ast/Expressions/ForLoopExpression.cs
+++ src/Ast/Expressions/ForLoopExpression.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Ast.Statements;
-
-namespace Ast.Expressions;
+﻿namespace Ast.Expressions;
 
 public sealed class ForLoopExpression : Expression
 {
