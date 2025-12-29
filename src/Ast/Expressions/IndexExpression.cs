25e677eb720683a90c27cc7ea752e8af957868d9
Sakata <pichugov-sasha@mail.ru>
2025-12-29 12:06:45 +0300

    Разработчик: обновил Gherkin-тесты

---
+++ src/Ast/Expressions/IndexExpression.cs
@@ -0,0 +1,25 @@
+﻿namespace Ast.Expressions;
+
+public sealed class IndexExpression : Expression
+{
+    public IndexExpression(Expression target, Expression index)
+    {
+        Target = target;
+        Index = index;
+    }
+
+    /// <summary>
+    /// Выражение, к которому применяется индексация (должно иметь тип ЦИТАТА).
+    /// </summary>
+    public Expression Target { get; }
+
+    /// <summary>
+    /// Выражение индекса (должно иметь тип ЦИФЕРКА).
+    /// </summary>
+    public Expression Index { get; }
+
+    public override void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
